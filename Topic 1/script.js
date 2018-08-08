d30e067524b890e0915d1cf82a0d98191ba30503
Soul <feedeurrutia@gmail.com>
2018-08-07 23:31:42 -0300

    Changed fetch to jQuery.ajax

--- Topic 1/script.js
+++ Topic 1/script.js
@@ -5,11 +5,12 @@ function test() {
 	alert('Hello world');
 	};
 $('#button').click(test);
-
-fetch('http://api.icndb.com/jokes/random')
-	.then(function(response) {
-		return response.json();
-  	})
-  	.then(function(response) {
-    	$('#section').html(response.value['joke']);
-  	});
+$.ajax({
+        url: "http://api.icndb.com/jokes/random",
+        success: function(response) {
+            $('#section').html(response.value['joke']);
+        },
+        error: function(error) {
+            $("#section").css("color", "red");
+        }
+    });
