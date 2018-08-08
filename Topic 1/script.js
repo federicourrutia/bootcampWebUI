2306872d41ae90f4c994c5f7098925cc01dbfeda
Soul <feedeurrutia@gmail.com>
2018-08-07 22:58:34 -0300

    Adapted to jQuery library

--- Topic 1/script.js
+++ Topic 1/script.js
@@ -1,15 +1,15 @@
-window.onload = function() {
-	document.getElementsByClassName('hidden')[0].classList.remove('hidden');
-	};
+$(document).ready(function() {
+	$('.hidden').removeClass('hidden');
+	});
 function test() {
 	alert('Hello world');
 	};
-document.getElementById('button').addEventListener('click', test);
+$('#button').click(test);
 
 fetch('http://api.icndb.com/jokes/random')
 	.then(function(response) {
-		    return response.json();
+		return response.json();
   	})
   	.then(function(response) {
-    	document.getElementById('section').innerHTML = response.value['joke'];
-  	})
+    	$('#section').html(response.value['joke']);
+  	});
